414de69919132d7bcc731011d3b5cd3f27a8ce63
DayMartin <negociosdinah@gmail.com>
2024-05-20 21:36:34 -0300

    fix: adicionando novos componentes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RecebiveisModule } from './recebiveis/recebiveis.module';
+import { CedentesModule } from './cedentes/cedentes.module';
+import { IntegracaoModule } from './integracao/integracao.module';
 
 @Module({
-  imports: [],
+  imports: [RecebiveisModule, CedentesModule, IntegracaoModule],
   controllers: [AppController],
   providers: [AppService],
 })
